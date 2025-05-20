7c6e5ce262825ddb04604bb04399571eb44b4f15
sugimotoshoki4959 <sugimoto.shoki.4959@members.co.jp>
2025-05-20 12:14:51 +0900

    残りjsが絡む部分のみ

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    // console.log('テスト');
+
+    /* slick-mv */
+    $('.mv_topre').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: false,
+        pauseOnFocus: false
+    });
+});
