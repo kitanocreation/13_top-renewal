3470ed7e7ac7a8b6c6f6603a852d93a3db927ce9
sugimotoshoki4959 <sugimoto.shoki.4959@members.co.jp>
2025-05-26 04:01:33 +0900

    残りデータ挿入と微調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,57 +11,44 @@ $(function() {
     let scrollUpCounter = 0;
     let scrollDownCounter = 0;
     $(window).on('scroll', function() {
-
         const currentScrollTop = $(this).scrollTop();
         if(currentScrollTop < TOP_SECTION_THRESHOLD) {
-
             $header.addClass('close_topre');
             scrollUpCounter = 0;
             scrollDownCounter = 0;
         } else {
-
             if(currentScrollTop > lastScrollTop) {
-
                 scrollDownCounter += (currentScrollTop - lastScrollTop);
                 scrollUpCounter = 0;
                 if(scrollDownCounter >= SCROLL_THRESHOLD) {
-
                     $header.addClass('close_topre');
                     scrollDownCounter = 0;
                 }
             } else if(currentScrollTop < lastScrollTop) {
-
                 scrollUpCounter += (lastScrollTop - currentScrollTop);
                 scrollDownCounter = 0;
                 if(scrollUpCounter >= SCROLL_THRESHOLD) {
-
                     $header.removeClass('close_topre');
                     scrollUpCounter = 0;
                 }
             }
         }
-
         if(currentScrollTop <= TOP_SECTION_THRESHOLD) {
-
             $guideTopre.css('margin-top', '136px');
         } else {
-
             $guideTopre.css('margin-top', '110px');
         }
         lastScrollTop = currentScrollTop;
     });
     const initialScrollTop = $(window).scrollTop();
     if(initialScrollTop < TOP_SECTION_THRESHOLD) {
-
         $header.addClass('close_topre');
     } else {
         $header.removeClass('close_topre');
     }
     if(initialScrollTop <= TOP_SECTION_THRESHOLD) {
-
         $guideTopre.css('margin-top', '136px');
     } else {
-
         $guideTopre.css('margin-top', '110px');
     }
 
@@ -70,33 +57,37 @@ $(function() {
         $(this).toggleClass('gnav-select_topre');
         $(this).find('nav.medium_topre').toggleClass('medium-select_topre');
     });
+    $('li.catgory_topre').hover(function() {
+        $('nav.medium_topre > ul > li').on('click', function() {
+            $('nav.small_topre').removeClass('small-select_topre');
+            $(this).find('nav.small_topre').addClass('small-select_topre');
+        });
+        $('nav.breadcrumb_topre').on('click', function(event) {
+            event.stopPropagation();
+            $(this).parents('nav.small-select_topre').removeClass('small-select_topre');
+        });
+    });
 
     /* drawer_topre */
     $('li.drawerbutton_topre').on('click', function() {
-
         const $this1 = $(this).find('img');
         const $this2 = $(this).find('span');
         const closeIconSrc = 'https://gigaplus.makeshop.jp/kitanoace/19_top-renewal/assets/img/template/header/close-icon.svg';
         const originalIconSrc = 'https://gigaplus.makeshop.jp/kitanoace/19_top-renewal/assets/img/template/header/dmenu-icon.svg';
         $('nav.drawer_topre').toggleClass('open_topre');
         if($('nav.drawer_topre').hasClass('open_topre')) {
-
             $this1.attr('src', closeIconSrc);
             $this2.text('閉じる');
         } else {
-
             $this1.attr('src', originalIconSrc);
             $this2.text('メニュー');
         }
     });
     $('ul.category_topre li').on('click', function() {
-
         if ($(this).hasClass('open_topre')) {
-
             $(this).removeClass('open_topre');
             $(this).find('.large_topre span:eq(1)').text('+');
         } else {
-
             $('ul.category_topre li').each(function() {
                 $(this).removeClass('open_topre');
                 $(this).find('.large_topre span:eq(1)').text('+');
@@ -115,16 +106,39 @@ $(function() {
         pauseOnFocus: false
     });
 
+    /* ranking_topre */
+    $('p.ranking-icon_topre img').each(function(index) {
+        let srcToSet;
+        if(index === 0) {
+            srcToSet = 'https://gigaplus.makeshop.jp/kitanoace/19_top-renewal/assets/img/ranking1-icon.svg'; // 1つ目のliの場合
+        } else if(index === 1) {
+            srcToSet = 'https://gigaplus.makeshop.jp/kitanoace/19_top-renewal/assets/img/ranking2-icon.svg'; // 2つ目のliの場合
+        } else if(index === 2) {
+            srcToSet = 'https://gigaplus.makeshop.jp/kitanoace/19_top-renewal/assets/img/ranking3-icon.svg'; // 3つ目のliの場合
+        } else {
+            srcToSet = 'https://gigaplus.makeshop.jp/kitanoace/19_top-renewal/assets/img/ranking4-icon.svg'; // 4つ目以降のliの場合
+        }
+        $(this).attr('src', srcToSet);
+    });
+
     /*  */
     $('main.top_topre button').on('click', function() {
         if($('main.top_topre ul').hasClass('open_topre')) {
             $('main.top_topre ul').removeClass('open_topre');
             $('main.top_topre button').removeClass('open_topre');
-            $(this).parent('section').find('ul').addClass('open_topre');
-            $(this).parent('section').find('button').addClass('open_topre');
+            $(this).parents('section').find('ul').addClass('open_topre');
+            $(this).parents('section').find('button').addClass('open_topre');
         } else {
-            $(this).parent('section').find('ul').addClass('open_topre');
-            $(this).parent('section').find('button').addClass('open_topre');
+            $(this).parents('section').find('ul').addClass('open_topre');
+            $(this).parents('section').find('button').addClass('open_topre');
+        }
+    });
+    $('button.next-icon_topre').on('click', function() {
+        $(this).parents('section').addClass('active_topre');
+    });
+    $('button.prev-icon_topre').on('click', function() {
+        if($(this).parents('section').hasClass('active_topre')) {
+            $(this).parents('section').removeClass('active_topre');
         }
     });
 
@@ -134,7 +148,6 @@ $(function() {
         const maxLength = parseInt($p.data('max-length'), 10);
         const originalText = $p.text();
         if(originalText.length > maxLength) {
-        
             const truncatedText = originalText.slice(0, maxLength) + '...';
             $p.text(truncatedText);
         }
